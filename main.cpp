e6c214bbb9803be1ed3d1f75ecc65ddc0912073c
Justice Parham <justking100@gmail.com>
2020-10-09 17:20:52 -0400

    Parell commands initial try

--- main.cpp
+++ main.cpp
@@ -19,11 +19,18 @@ using namespace std;
 void printShell();
 std::string getLineInput();
 void runSHellInput();
-static int exec_prog(const char **argv);
+
+static int exec_prog(const char **argv, bool waitForCompletion);
 const char** getArgVfromString(const char* args);
-void handlePromptLine(string basic_string);
+
+void handlePromptLine(string raw_command);
 void signal_callback_handler(int signum);
 
+std::string trim(const std::string &str);
+
+void execute_programs(const string &pString);
+
+int splitParalell(const char *pString[], const string &somethingelse);
 
 int main(int argc, char *argv[]) {
     signal(SIGINT, signal_callback_handler);
@@ -31,16 +38,18 @@ int main(int argc, char *argv[]) {
     while ((opt = getopt(argc, argv, "")) != -1) {
         switch (opt) {
             default:
-                fprintf(stderr, "Usage: %s [>> file...]\n", argv[0]);
+                fprintf(stderr, "Usage: %s [file...]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
 
     }
+
     printShell();
     while (true){
         runSHellInput();
     }
 }
+
 
 void signal_callback_handler(int signum) {
     cout << endl << "Terminating Shell With Exit Code: " << signum << endl;
@@ -54,15 +63,69 @@ void runSHellInput(){
 
 }
 
-void handlePromptLine(string basic_string) {
-
-    if (basic_string == "exit"){
+void handlePromptLine(string raw_command) {
+    if (raw_command == "exit") {
         exit(EXIT_SUCCESS);
     }
     else{
-
-        exec_prog(getArgVfromString(basic_string.c_str()));
+        execute_programs(raw_command);
     }
+
+}
+
+std::string trim(const std::string &str) {
+
+
+    string whitespace = "\t";
+    const auto strBegin = str.find_first_not_of(whitespace);
+    if (strBegin == std::string::npos) {
+        return "";
+    } // no content
+    const auto strEnd = str.find_last_not_of(whitespace);
+    const auto strRange = strEnd - strBegin + 1;
+
+    return str.substr(strBegin, strRange);
+}
+
+void execute_programs(const string &pString) {
+    const char **programsToExecute = static_cast<const char **>(malloc(10 * sizeof(char *)));
+    for (int i = 0; i < 30; ++i)
+        programsToExecute[i] = static_cast<const char *>(malloc(20 * sizeof(char)));
+
+    int numberAdded = splitParalell(programsToExecute, pString);
+    int pids[55];
+    if (numberAdded > 1) {
+        cout << endl;
+        printf("Added %i commands ", numberAdded);
+        cout << endl;
+        for (int i = 0; i < numberAdded; i++) {
+            printf("Executing: [%s]", programsToExecute[i]);
+            cout << endl;
+        }
+        for (int i = 0; i < numberAdded; i++) {
+            const char **argV = getArgVfromString(programsToExecute[i]);
+            for (int j = 0; j < sizeof(argV); j++) {
+                cout << endl;
+                printf("[%s]", argV[j]);
+            }
+            pids[i] = exec_prog(argV, false);
+        }
+    }
+    for (int i = 0; i < numberAdded; i++) {
+        wait(&pids[i]);
+    }
+    exec_prog(getArgVfromString(pString.c_str()), true);
+
+}
+
+int splitParalell(const char *pString[], const string &somethingelse) {
+    int i = 0;
+    char *string1 = strdup(somethingelse.c_str());
+    while ((pString[i] = trim((string) strsep(&string1, "&")).c_str()) != NULL) {
+        i++;
+
+    }
+    return i;
 
 }
 
@@ -86,7 +149,8 @@ const char** getArgVfromString(const char* args) {
     }
     return sub_str;
 }
-static int exec_prog(const char **argv)
+
+static int exec_prog(const char **argv, bool waitForCompletion)
 {
     pid_t   my_pid;
     int     status, timeout /* unused ifdef WAIT_FOR_COMPLETION */;
@@ -95,30 +159,12 @@ static int exec_prog(const char **argv)
         strncpy(buffer, "/bin/", sizeof(buffer));
         strncat(buffer,  argv[0], sizeof(buffer));
         if (-1 == execve(buffer, (char **)argv , NULL)) {
-            perror("child process execve failed [%m]");
+            perror("child process execve failed");
             return -1;
         }
     }
-    wait(&my_pid);
-#ifdef WAIT_FOR_COMPLETION
-    timeout = 1000;
+    if (waitForCompletion)
+        wait(&my_pid);
 
-    while (0 == waitpid(my_pid , &status , WNOHANG)) {
-            if ( --timeout < 0 ) {
-                    perror("timeout");
-                    return -1;
-            }
-            sleep(1);
-    }
-
-    printf("%s WEXITSTATUS %d WIFEXITED %d [status %d]\n",
-            argv[0], WEXITSTATUS(status), WIFEXITED(status), status);
-
-    if (1 != WIFEXITED(status) || 0 != WEXITSTATUS(status)) {
-            perror("%s failed, halt system");
-            return -1;
-    }
-
-#endif
-    return 0;
+    return my_pid;
 }
